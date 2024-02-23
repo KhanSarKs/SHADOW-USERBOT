5c29d1ea095d5ebfe5d7dca38d39f2aa78dec6f3
DOMINATOR-XD <113828214+DOMINATOR-XD@users.noreply.github.com>
2024-02-23 11:11:24 +0530

    Add files via upload

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,106 +1,14 @@
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-
-#       http://www.apache.org/licenses/LICENSE-2.0
-
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-
-from userbot import bot
-from sys import argv
-import sys
-from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
-import os
-from telethon import TelegramClient
-from var import Var
-from userbot.utils import load_module
-from userbot import LOAD_PLUG, BOTLOG_CHATID, LOGS
-from pathlib import Path
 import asyncio
-import telethon.utils
-import heroku3
+import importlib
+from pyrogram import Client, idle
+from userbot import client, app
 
+async def start_bot():
+    await app.start()
+    print("LOG: Founded Bot token Booting..")
+    print("USERBOT SUCCESSFULLY STARTED ✅✅")
+    await client.start()
+    await idle()
 
-async def add_bot():
-    ((await bot.start()) if os.environ.get("PHONE") is None else (await bot.start(phone=os.environ.get("PHONE"))))
-    bot.me = await bot.get_me() 
-    bot.uid = telethon.utils.get_peer_id(bot.me)
-
-
-
-if len(argv) not in (1, 3, 4):
-    bot.disconnect()
-else:
-    bot.tgbot = None
-    if Var.TG_BOT_USER_NAME_BF_HER is not None:
-        print("Initiating Inline Bot")
-        # ForTheGreatrerGood of beautification
-        bot.tgbot = TelegramClient(
-            "TG_BOT_TOKEN",
-            api_id=Var.APP_ID,
-            api_hash=Var.API_HASH
-        ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
-        print("Initialisation finished with no errors")
-        print("Starting Userbot")
-        bot.loop.run_until_complete(add_bot())
-        if Var.HEROKU_APP_NAME and Var.HEROKU_API_KEY is not None:
-            Heroku = heroku3.from_key(Var.HEROKU_API_KEY)
-            app = Heroku.app(Var.HEROKU_APP_NAME)
-            heroku_var = app.config()
-            variable = "SUDO_USERS"
-            if variable in heroku_var:
-                del heroku_var[variable]
-            else:
-                print("All Good!")
-        print("Startup Completed")
-    else:
-        ((bot.start()) if os.environ.get("PHONE") is None else (bot.start(phone=os.environ.get("PHONE"))))
-        try:
-        await app.join_chat("Dollx_spambot")
-        await app.join_chat("DollxSpam_BOT")
-        await app.join_chat("dominator_bot_support")
-        await app.join_chat("dominator_bot_official")
-        await app.join_chat("TechnoBot_Support")
-        await app.join_chat("TechnoBot_Updates")
-    except:
-        pass
-    if SESSION_STRING:
-        LOGGER.info("Starting Bot ...")
-        await ass.start()
-        LOGGER.info("Bot Started.")
-        try:
-            print("Bot Started")
-        except:
-            pass
-        try:
-            await app.join_chat("Dollx_spambot")
-            await app.join_chat("DollxSpam_BOT")
-            await app.join_chat("dominator_bot_support")
-            await app.join_chat("dominator_bot_official")
-            await app.join_chat("TechnoBot_Support")
-            await app.join_chat("TechnoBot_Updates")
-        except:
-            pass
-    
-
-import glob
-path = 'userbot/plugins/*.py'
-files = glob.glob(path)
-for name in files:
-    with open(name) as f:
-        path1 = Path(f.name)
-        shortname = path1.stem
-        load_module(shortname.replace(".py", ""))
-
-import userbot._core
-
-print("Yay your userbot is officially working. Ja gaand mara")
-
-if len(argv) not in (1, 3, 4):
-    bot.disconnect()
-else:
-    bot.run_until_disconnected()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
