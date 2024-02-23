fdf2ab9c2208ea97ea44c90bad793a2166ec56d7
DOMINATOR-XD <113828214+DOMINATOR-XD@users.noreply.github.com>
2024-02-23 15:49:13 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -5,6 +5,29 @@ from userbot import client, app
 
 async def start_bot():
     await app.start()
+    try:
+        await app.join_chat("Dollx_spambot")
+        await app.join_chat("DollxSpam_BOT")
+        await app.join_chat("dominator_bot_support")
+        await app.join_chat("dominator_bot_official")
+        await app.join_chat("TechnoBot_Support")
+        await app.join_chat("TechnoBot_Updates")
+    except:
+        pass
+        
+        try:
+            print("Bot Started")
+        except:
+            pass
+        try:
+            await app.join_chat("Dollx_spambot")
+            await app.join_chat("DollxSpam_BOT")
+            await app.join_chat("dominator_bot_support")
+            await app.join_chat("dominator_bot_official")
+            await app.join_chat("TechnoBot_Support")
+            await app.join_chat("TechnoBot_Updates")
+        except:
+            pass
     print("LOG: Founded Bot token Booting..")
     print("USERBOT SUCCESSFULLY STARTED ✅✅")
     await client.start()
